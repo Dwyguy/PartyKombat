aa3bcfc7f4876eddf374ac91041a7babccd5393a
Bneibeck <Bneibeck@gmail.com>
2015-01-24 20:45:30 -0500

    Castle And Dragon
    Castle Model and animated dragon added

---
+++ PartyKombat/Assets/FBX/Dragon/DragonRun.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragonRun : MonoBehaviour {
+	public Animator []dragon;
+
+
+	// Use this for initialization
+	void Start () {
+		print ("DragonRun Script active");
+		dragon = GetComponentsInChildren<Animator> ();
+		dragon[0].SetBool ("Run", true);
+		dragon[0].applyRootMotion = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
